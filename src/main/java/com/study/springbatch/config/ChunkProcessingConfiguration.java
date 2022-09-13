074cfe990fae6b13f30055f22a464ff5e93a7650
이재민 <jmlee@commerce-league.com>
2022-09-13 21:27:38 +0900

    chunk 기반의 배치

--- src/main/java/com/study/springbatch/config/ChunkProcessingConfiguration.java
+++ src/main/java/com/study/springbatch/config/ChunkProcessingConfiguration.java
@@ -10,6 +10,10 @@ import org.springframework.batch.core.configuration.annotation.JobBuilderFactory
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.support.ListItemReader;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -27,9 +31,34 @@ public class ChunkProcessingConfiguration {
         return jobBuilderFactory.get("chunkProcessingJob")
             .incrementer(new RunIdIncrementer())
             .start(this.taskBaseStep())
+            .next(this.chunkBaseStep())
             .build();
     }
 
+    @Bean
+    public Step chunkBaseStep() {
+        return stepBuilderFactory.get("chunckBaseStep")
+            .<String, String>chunk(10)
+            .reader(itemReader())
+            .processor(itemProcessor())
+            .writer(itemWriter())
+            .build();
+    }
+
+    private ItemWriter<String> itemWriter() {
+        return items -> log.info("chunk item size : {}", items.size());
+    }
+
+    // processor는 reader에서 읽은 데이터를 가공, writer로 데이터를 전달할지 말지 결정
+    private ItemProcessor<String, String> itemProcessor() {
+        return item -> item + ", spring batch";
+    }
+
+    private ItemReader<String> itemReader() {
+        return new ListItemReader<>(getItems());
+    }
+
+    // Reader 가 null 을 리턴할 때 까지 반복
     @Bean
     public Step taskBaseStep() {
         return stepBuilderFactory.get("taskBaseStep")
