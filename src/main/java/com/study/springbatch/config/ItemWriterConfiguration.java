45f6138263ad60145ba3e5e94a2df45237bacc69
이재민 <jmlee@commerce-league.com>
2022-09-22 23:29:24 +0900

    JdbcItemWriter를 이용해 테이블에 데이터 삽입

--- src/main/java/com/study/springbatch/config/ItemWriterConfiguration.java
+++ src/main/java/com/study/springbatch/config/ItemWriterConfiguration.java
@@ -2,6 +2,7 @@ package com.study.springbatch.config;
 
 import java.util.ArrayList;
 import java.util.List;
+import javax.sql.DataSource;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
@@ -11,6 +12,9 @@ import org.springframework.batch.core.configuration.annotation.StepBuilderFactor
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.database.BeanPropertyItemSqlParameterSourceProvider;
+import org.springframework.batch.item.database.JdbcBatchItemWriter;
+import org.springframework.batch.item.database.builder.JdbcBatchItemWriterBuilder;
 import org.springframework.batch.item.file.FlatFileItemWriter;
 import org.springframework.batch.item.file.builder.FlatFileItemWriterBuilder;
 import org.springframework.batch.item.file.transform.BeanWrapperFieldExtractor;
@@ -26,12 +30,14 @@ public class ItemWriterConfiguration {
 
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
+    private final DataSource dataSource;
 
     @Bean
     public Job itemWriterJob() throws Exception {
         return this.jobBuilderFactory.get("itemWriterJob")
             .incrementer(new RunIdIncrementer())
             .start(this.csvItemWriterStep())
+            .next(this.jdbcBatchItemWriterStep())
             .build();
     }
 
@@ -42,6 +48,27 @@ public class ItemWriterConfiguration {
             .reader(itemReader())
             .writer(csvFileItemWriter())
             .build();
+    }
+
+    @Bean
+    public Step jdbcBatchItemWriterStep() {
+        return stepBuilderFactory.get("jdbcBatchItemWriterStep")
+            .<Person, Person>chunk(10)
+            .reader(itemReader())
+            .writer(jdbcBatchItemWriter())
+            .build();
+    }
+
+    private ItemWriter<Person> jdbcBatchItemWriter() {
+        JdbcBatchItemWriter<Person> itemWriter = new JdbcBatchItemWriterBuilder<Person>()
+            .dataSource(dataSource)
+            .itemSqlParameterSourceProvider(new BeanPropertyItemSqlParameterSourceProvider<>())
+            .sql("insert into person(name, age, address) values(:name, :age, :address)")
+            .build();
+
+        itemWriter.afterPropertiesSet();
+
+        return itemWriter;
     }
 
     private ItemWriter<Person> csvFileItemWriter() throws Exception {
